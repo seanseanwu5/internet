cff912fd0515b0e8744981df6bf757ecc9268be0
unknown <seanseanwu5@gmail.com>
2024-11-21 02:02:18 +0800

    Initial commit

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 # Backend server using Flask and Socket.IO
 
 import eventlet
-eventlet.monkey_patch(dns=False)
+eventlet.monkey_patch()
 
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # 啟動 Flask 應用
-gunicorn --bind 0.0.0.0:5000 server:app
+# gunicorn --bind 0.0.0.0:5000 server:app
+gunicorn -k eventlet server:app
