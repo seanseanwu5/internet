8155a097cf125e416c31fb6d344f8f84eabef90c
unknown <seanseanwu5@gmail.com>
2024-11-21 11:21:50 +0800

    Initial commit

--- server.py
+++ server.py
@@ -252,4 +252,5 @@ if __name__ == '__main__':
 #     socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 if __name__ == '__main__':
     import os
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
