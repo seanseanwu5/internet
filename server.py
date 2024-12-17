80f923eecf2876c1ac6bbd4f5e3d64d527fc4f7c
unknown <seanseanwu5@gmail.com>
2024-12-18 00:04:16 +0800

    Initial commit

--- server.py
+++ server.py
@@ -271,6 +271,5 @@ def check_bingo(marked):
 #     socketio.run(app, debug=True)
 if __name__ == '__main__':
     import os
-    # Render 会通过环境变量 PORT 指定动态端口
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
